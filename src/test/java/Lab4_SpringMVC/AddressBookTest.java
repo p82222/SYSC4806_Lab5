73f3d6db8704ed8f6552529400703f38028dc0e6
p82222 <p82222@yahoo.com.tw>
2023-10-16 14:28:01 -0400

    step 5 test

--- src/test/java/Lab4_SpringMVC/AddressBookTest.java
+++ src/test/java/Lab4_SpringMVC/AddressBookTest.java
@@ -12,14 +12,15 @@ class AddressBookTest {
 
         BuddyInfo buddy1 = new BuddyInfo("John", "123-456-7890");
         BuddyInfo buddy2 = new BuddyInfo("Alice", "987-654-3210");
+        BuddyInfo buddy3 = new BuddyInfo("Buddy3", "333-333-3333");
 
         addressBook.addBuddy(buddy1);
         addressBook.addBuddy(buddy2);
 
-        Assertions.assertEquals(2, addressBook.getBuddyList().size());
+        Assertions.assertEquals(3, addressBook.getBuddyList().size());
 
         addressBook.removeBuddy(buddy1);
 
-        Assertions.assertEquals(1, addressBook.getBuddyList().size());
+        Assertions.assertEquals(2, addressBook.getBuddyList().size());
     }
 }
